87947c8247cba9e35e045ff60552f86059a7f77f
Zzebastian <sebastian.scheverin@gmail.com>
2023-12-25 10:03:49 -0300

    intento casi funcional

---
+++ Zzebaz/main.py
@@ -0,0 +1,21 @@
+﻿from reactpy import component, html, run
+contador = 1
+
+@component
+def accion(texto, mensaje):
+    def handle_event(event):
+        global contador
+        contador+=1
+        print(contador)
+        return html.button({"on_click": handle_event}, f"ahora vale {contador}")
+
+    return html.button({"on_click": handle_event}, f"ahora vale {contador}")
+
+
+
+def lazo():
+    return html.div(accion(f"ahora vale {contador}", "se guardó"))
+    
+
+
+run(lazo)
